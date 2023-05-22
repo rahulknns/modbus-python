f9857b614180fe51e07e5cb5828e7f9ab662b22c
Rahulkannan S <rahulknns@gmail.com>
2023-05-22 16:44:02 +0530

    client complete

--- modbus_rtu/client.py
+++ modbus_rtu/client.py
@@ -1,18 +1,8 @@
-from modbus_frames import response_frames
+from modbus_frames import request_frames
 from modbus_frames import functions_codes as fc
-import serial
+from modbus_rtu import crc
+from serial import Serial
 
-def crc(data):
-    crc = 0xFFFF
-    for byte in data:
-        crc ^= byte
-        for i in range(8):
-            if crc & 0x0001:
-                crc >>= 1
-                crc ^= 0xA001
-            else:
-                crc >>= 1
-    return crc.to_bytes(2, byteorder="big")
 
 class ModbusRtuClient():
     def __init__(self,server_address, port, baudrate, timeout):
@@ -20,114 +10,233 @@ class ModbusRtuClient():
         self._port = port
         self._baudrate = baudrate
         self._timeout = timeout
-        self._serial = serial.Serial(port, baudrate, timeout=timeout)            
 
-
-        
-    def read_coils(self, server_address,start_address, quantity):
-        self._server_address = server_address
-        request = request_frames.read_coils(start_address,quantity)
-        request = server_address.to_bytes(2,byteorder="big") + request + crc(request)
-        self._serial.write(request)
-        response = self._serial.read(5 + quantity // 8 + (1 if quantity % 8 else 0))
-        if response[0] != server_address:
-            raise Exception("Wrong server address")
-        if response[1] != fc.read_coils:
-            raise Exception("Wrong function code")
-        if response[2] != quantity // 8 + (1 if quantity % 8 else 0):
-            raise Exception("Wrong byte count")
-        if crc(response[:-2]) != response[-2:]:
-            raise Exception("Wrong CRC")
-        return response[3:-2]
+    def read_coils(self,start_address,quantity):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.read_coils(start_address,quantity)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
+        byte_count = quantity//8 + (1 if quantity%8 else 0)
+        response = self._serial.read( 5 + byte_count)
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return response
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return None
+        if (response[1] != fc.read_coils):
+            print("function code doesn't match")
+            return None
+        if (response[2] != byte_count):
+            print(" Wrong Byte count")
+            return None
+        values = []
+        data = response[3:-2]
+        for byt_e,byte_no in zip(data,range(len(data))):
+            for bit in range(0,8):
+                if (quantity >= byte_no*8 + bit + 1):
+                    values.append( (byt_e & 2**bit) >> bit )
+        return values
     
-    def read_discrete_inputs(self, server_address,start_address, quantity):
-        self._server_address = server_address
-        request = request_frames.read_discrete_inputs(start_address,quantity)
-        request = server_address.to_bytes(2,byteorder="big") + request + crc(request)
-        self._serial.write(request)
-        response = self._serial.read(5 + quantity // 8 + (1 if quantity % 8 else 0))
-        if response[0] != server_address:
-            raise Exception("Wrong server address")
-        if response[1] != fc.read_discrete_inputs:
-            raise Exception("Wrong function code")
-        if response[2] != quantity // 8 + (1 if quantity % 8 else 0):
-            raise Exception("Wrong byte count")
-        if crc(response[:-2]) != response[-2:]:
-            raise Exception("Wrong CRC")
-        return response[3:-2]
+    def read_discrete_inputs(self,start_address,quantity):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.read_discrete_inputs(start_address,quantity)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
+        byte_count = quantity//8 + (1 if quantity%8 else 0)
+        response = self._serial.read( 5 + byte_count)
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return response
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return None
+        if (response[1] != fc.read_discrete_inputs):
+            print("function code doesn't match")
+            return None
+        if (response[2] != byte_count):
+            print(" Wrong Byte count")
+            return None
+        values = []
+        data = response[3:-2]
+        for byt_e,byte_no in zip(data,range(len(data))):
+            for bit in range(0,8):
+                if (quantity >= byte_no*8 + bit + 1):
+                    values.append( (byt_e & 2**bit) >> bit )
+        return values
     
-    def read_holding_registers(self, server_address,start_address, quantity):
-        self._server_address = server_address
-        request = request_frames.read_holding_registers(start_address,quantity)
-        request = server_address.to_bytes(2,byteorder="big") + request + crc(request)
-        self._serial.write(request)
-        response = self._serial.read(5 + quantity * 2)
-        if response[0] != server_address:
-            raise Exception("Wrong server address")
-        if response[1] != fc.read_holding_registers:
-            raise Exception("Wrong function code")
-        if response[2] != quantity * 2:
-            raise Exception("Wrong byte count")
-        if crc(response[:-2]) != response[-2:]:
-            raise Exception("Wrong CRC")
-        return response[3:-2]
 
-    def read_input_registers(self, server_address,start_address, quantity):
-        self._server_address = server_address
-        request = request_frames.read_input_registers(start_address,quantity)
-        request = server_address.to_bytes(2,byteorder="big") + request + crc(request)
-        self._serial.write(request)
-        response = self._serial.read(5 + quantity * 2)
-        if response[0] != server_address:
-            raise Exception("Wrong server address")
-        if response[1] != fc.read_input_registers:
-            raise Exception("Wrong function code")
-        if response[2] != quantity * 2:
-            raise Exception("Wrong byte count")
-        if crc(response[:-2]) != response[-2:]:
-            raise Exception("Wrong CRC")
-        return response[3:-2]
+    def read_holding_registers(self,start_address,quantity):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.read_holding_registers(start_address,quantity)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
+        byte_count = 2*quantity
+        response = self._serial.read( 5 + byte_count)
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return response
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return None
+        if (response[1] != fc.read_holding_registers):
+            print("function code doesn't match")
+            return None
+        if (response[2] != byte_count):
+            print(" Wrong Byte count")
+            return None
+        values = []
+        data = response[3:-2]
+        for register_no in range(len(quantity)):
+            value = (data[2*register_no]<<8) + data[2*register_no + 1]
+            values.append(value)
+            
+        return values
+    
+    def read_input_registers(self,start_address,quantity):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.read_input_registers(start_address,quantity)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
+        byte_count = 2*quantity
+        response = self._serial.read( 5 + byte_count)
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return response
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return None
+        if (response[1] != fc.read_holding_registers):
+            print("function code doesn't match")
+            return None
+        if (response[2] != byte_count):
+            print(" Wrong Byte count")
+            return None
+        values = []
+        data = response[3:-2]
+        for register_no in range(len(quantity)):
+            value = (data[2*register_no]<<8) + data[2*register_no + 1]
+            values.append(value)
+            
+        return values
 
-    def write_single_coil(self, server_address,address, value):
-        self._server_address = server_address
-        request = request_frames.write_single_coil(address,value)
-        request = server_address.to_bytes(2,byteorder="big") + request + crc(request)
-        self._serial.write(request)
+    def write_single_coil(self,address,value):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.write_single_coil(address,value)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
         response = self._serial.read(8)
-        if response[0] != server_address:
-            raise Exception("Wrong server address")
-        if response[1] != fc.write_single_coil:
-            raise Exception("Wrong function code")
-        if crc(response[:-2]) != response[-2:]:
-            raise Exception("Wrong CRC")
-        return response[3:-2]
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return False
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return False
+        if (response[1] != fc.write_single_coil):
+            print("Invalid function code recieved")
+            return False
+        if ((response[2]<<8 + response[3]) != address):
+            print("Invalid coil address recieved")
+            return False
+        if (value):
+            if ( not (response[4] == 0xFF and response[5] == 0x00) ):
+                print("Invalid value recieved")
+                return False
+        elif ( not (response[4] == 0x00 and response[5] == 0x00) ):
+                print("Invalid value recieved")
+                return False
+        return True
     
-    def write_single_register(self, server_address,address, value):
-        self._server_address = server_address
-        request = request_frames.write_single_register(address,value)
-        request = server_address.to_bytes(2,byteorder="big") + request + crc(request)
-        self._serial.write(request)
+    def write_single_register(self,address,value):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.write_single_register(address,value)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
         response = self._serial.read(8)
-        if response[0] != server_address:
-            raise Exception("Wrong server address")
-        if response[1] != fc.write_single_register:
-            raise Exception("Wrong function code")
-        if crc(response[:-2]) != response[-2:]:
-            raise Exception("Wrong CRC")
-        return response[3:-2]
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return False
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return False
+        if (response[1] != fc.write_single_register):
+            print("Invalid function code recieved")
+            return False
+        if ((response[2]<<8 + response[3]) != address):
+            print("Invalid register address recieved")
+            return False
+        if ((response[4]<<8 + response[5]) != value):
+            print("Invalid value recieved")
+            return False
+        return True
     
-    def write_multiple_coils(self, server_address,start_address, values):
-        self._server_address = server_address
-        request = request_frames.write_multiple_coils(start_address,values)
-        request = server_address.to_bytes(2,byteorder="big") + request + crc(request)
-        self._serial.write(request)
+    def write_multiple_coils(self,start_address,quantity,values):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.write_multiple_coils(start_address,quantity,values)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
         response = self._serial.read(8)
-        if response[0] != server_address:
-            raise Exception("Wrong server address")
-        if response[1] != fc.write_multiple_coils:
-            raise Exception("Wrong function code")
-        if crc(response[:-2]) != response[-2:]:
-            raise Exception("Wrong CRC")
-        return response[3:-2]
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return False
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return False
+        if (response[1] != fc.write_multiple_registers):
+            print("Invalid function code recieved")
+            return False
+        if ((response[2]<<8 + response[3]) != start_address):
+            print("Invalid register address recieved")
+            return False
+        if ((response[4]<<8 + response[5]) != quantity):
+            print("Invalid quantity recieved")
+            return False
+        return True
+    
+
+    def write_multiple_coils(self,start_address,quantity,values):
+        request_adu = self._server_address.to_bytes(1,byteorder='big')
+        request_pdu = request_frames.write_multiple_registers(start_address,quantity,values)
+        request_adu += request_pdu
+        request_adu += crc.crc(request_adu)
+        self._serial = Serial(self._port,baudrate=self._baudrate,timeout=self._timeout)
+        self._serial.write(request_adu)
+        response = self._serial.read(8)
+        if (crc.crc(response[:-2]) != response[-2:]):
+            print(" CRC is not zero, discarding packets")
+            return False
+        if (response[0] != self._server_address):
+            print("Server address doesn't match")
+            return False
+        if (response[1] != fc.write_multiple_registers):
+            print("Invalid function code recieved")
+            return False
+        if ((response[2]<<8 + response[3]) != start_address):
+            print("Invalid register address recieved")
+            return False
+        if ((response[4]<<8 + response[5]) != quantity):
+            print("Invalid quantity recieved")
+            return False
+        return True
+
 
     
+
+
+    
+
+
